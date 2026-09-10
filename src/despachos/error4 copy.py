6c5a0658767268620453bf476bba97b6fc006e6c
alulab14 <alulab14@inf.pucp.edu.pe>
2026-09-10 18:13:02 -0500

    fix: final 2

--- src/despachos/error4 copy.py
+++ src/despachos/error4 copy.py
@@ -16,3 +16,41 @@ def malogrando:
     if (c < 0):
         print("Gano b")
     return c
+
+def malogrando2:
+    a = 3
+    b = 2
+    c  = 0
+    for i in range(1,30):
+        a = a*i + b
+        b = b*i + a
+        if (a > b):
+            c = c + 1
+        else:
+            c = c - 1
+    if (c > 0):
+        print("Gano a")
+    if (c == 0):
+        print("Empate")
+    if (c < 0):
+        print("Gano b")
+    return c
+
+def malogrando3:
+    a = 3
+    b = 2
+    c  = 0
+    for i in range(1,30):
+        a = a*i + b
+        b = b*i + a
+        if (a > b):
+            c = c + 1
+        else:
+            c = c - 1
+    if (c > 0):
+        print("Gano a")
+    if (c == 0):
+        print("Empate")
+    if (c < 0):
+        print("Gano b")
+    return c
